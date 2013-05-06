1f7c5202e3e60310ed94c6720de1b235a5fb9887
andrew <art26@zips.uakron.edu>
2013-05-06 18:25:29 -0400

    1st

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using BookCave.Service.Dto;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Web;
+using System.Web.Mvc;
+
+namespace root.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Results = new List<SuperDto>();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formCollection)
+        {
+            var author = formCollection["Author"];
+            var title = formCollection["Title"];
+            var skill = formCollection["Skill"];
+            var content = formCollection["Content"];
+            var summary = formCollection["Summary"];
+
+            //var uri = new Uri(String.Format(@"http://apps.apprenda.local/api/services/json/r/bookcavetest(v1)/BookService/IBook/books?author={0}&skill={1}&content={2}&title={3}&summary={4}",
+            var uri = new Uri(String.Format(@"http://localhost:40000/book.svc/books?author={0}&skill={1}&content={2}&title={3}&summary={4}",
+                author,
+                skill,
+                content,
+                title,
+                summary));
+            var webClient = new WebClient();
+            var resultBitStream = webClient.DownloadData(uri);
+            var serializedJson = new DataContractJsonSerializer(typeof(List<SuperDto>));
+            ViewBag.Results = serializedJson.ReadObject(new MemoryStream(resultBitStream));
+
+            return View();
+        }
+    }
+}
