5a3f67c8ebb6d9072b242b3ccd98b5cb629ba22d
andrew <art26@zips.uakron.edu>
2013-06-17 18:15:05 -0400

    azure for staging

---
+++ MvcWebRole1/Models/ViewModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookCaveClient.Models
+{
+    public class SearchParams
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+
+        public bool hasAwards { get; set; }
+
+        public int agelow { get; set; }
+        public int agehigh { get; set; }
+
+        public int skilllow { get; set; }
+        public int skillhigh { get; set; }
+
+        public string keywords { get; set; }
+
+        public string isbn13 { get; set; }
+
+    }
+
+    
+    public class UploadRequest
+    {
+        [DataType(DataType.MultilineText)]
+        public string lexilelines { get; set; }
+    }
+}
